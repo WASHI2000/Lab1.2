fda930bdc0c8379d75a8ed221ddf60f824495ff6
Mr-polo11polo11 <101180608+WASHI2000@users.noreply.github.com>
2022-03-22 10:11:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    int liczba = 1;
+    float ułamek = 3.14f;
+    char litera = 'b';
+    double ulamek2 = 3.123345456;
+        System.out.println(+liczba);
+        System.out.println(+ułamek);
+        System.out.println(+litera);
+        System.out.println(+ulamek2);
+  }
+}
